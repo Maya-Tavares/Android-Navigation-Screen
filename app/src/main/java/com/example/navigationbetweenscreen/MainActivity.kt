c54761a60be7562a102bd930db44add0c08dd9bc
Maya Tavares <94256355+Maya-Tavares@users.noreply.github.com>
2026-03-17 20:07:32 -0300

    Add LoginScreen na Main

--- app/src/main/java/com/example/navigationbetweenscreen/MainActivity.kt
+++ app/src/main/java/com/example/navigationbetweenscreen/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigationbetweenscreen.screens.LoginScreen
 import com.example.navigationbetweenscreen.ui.theme.NavigationbetweenscreenTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationbetweenscreenTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
